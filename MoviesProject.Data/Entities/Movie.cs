eff8a8dfb8fab6e0e8721e7a09b8916dc5b730ab
GeorgiMY <georgimyordanov07@gmail.com>
2025-11-14 15:30:14 +0200

    first commit

---
+++ MoviesProject.Data/Entities/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesProject.Data.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Director { get; set; } = null!;
+        public int Year { get; set; }
+        public string Genre { get; set; } = null!;
+    }
+}
---
+++ MoviesProject.Data/MovieContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoviesProject.Data.Entities;
+
+namespace MoviesProject.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) {}
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
